817eac3f3f1fd490e78a66185d623cd92ee76419
oliver <oliver.quail3@gmail.com>
2026-05-05 18:49:19 +1000

    Added additional libraries required for this task

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
+    annotationProcessor("androidx.room:room-compiler:2.8.4")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
