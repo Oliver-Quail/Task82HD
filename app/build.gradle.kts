12fe17b63b8f7f34881fa4c72755124a34c443d4
oliver <oliver.quail3@gmail.com>
2026-05-06 13:02:50 +1000

    Added items for navigation to phone layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.navigation:navigation-fragment:2.8.8")
+    implementation("androidx.navigation:navigation-ui:2.9.7")
+    implementation("androidx.navigation:navigation-compose:2.9.7")
 }
--- app/src/main/java/com/task82hd/MainActivity.java
+++ app/src/main/java/com/task82hd/MainActivity.java
@@ -7,6 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +30,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        FragmentContainerView fragmentHolder = findViewById(R.id.fragment_holder);
+
+        //AppBarConfiguration(new Set<T>(R.id.classify_fragment, R.id.history_fragment, R.id.classify_fragment));
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.classify_fragment, R.id.history_fragment, R.id.classify_fragment).build();
+
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_holder);
+
+        NavController navController = navHostFragment.getNavController();
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigation, navController);
+
+
+
     }
+
+
 }
