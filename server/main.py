3bec8a3324a2eba21e48e0bb4ed7c9a8d7cce5bb
oliver <oliver.quail3@gmail.com>
2026-05-15 17:27:18 +1000

    Further refinements to prompt

--- server/main.py
+++ server/main.py
@@ -29,7 +29,8 @@ def generate_classification(text, image):
 
     prompt = (
         f"Classify the following, using an image if avaliable and text. Respond with what bin it should be disposed of in or a question if you need more information"
-        f"Respond in this format of 'BIN:You should dispose of [item name] in the [BIN] bin' if classifying colour or 'QUESTION :[your question]' if you want to ask a question"
+        f"if you have any advice such as removing plastic wrapping from fruit place it in [Advice]"
+        f"Respond in this format of 'BIN:You should dispose of [item name] in the [BIN] bin. [Advice]' if classifying colour or 'QUESTION :[your question]' if you want to ask a question"
         f"The detail for the bins should be one of Green - organics, Yellow - recyclable plastic, Red - general waste, Blue - papaer "
         f""
         f"Information provided by user: " + gaurdPrompt(text)
