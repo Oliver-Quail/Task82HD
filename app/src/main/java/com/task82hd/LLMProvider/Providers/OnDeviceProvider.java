346229d97100992e38541ed1cb220ccb323057a9
oliver <oliver.quail3@gmail.com>
2026-05-16 16:01:14 +1000

    OnDeviceProvider.java now accepts text prompts and returns values

--- app/src/main/java/com/task82hd/LLMProvider/Providers/OnDeviceProvider.java
+++ app/src/main/java/com/task82hd/LLMProvider/Providers/OnDeviceProvider.java
@@ -3,16 +3,28 @@ package com.task82hd.LLMProvider.Providers;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.net.Uri;
+import android.util.Log;
 
 import com.google.ai.edge.litertlm.Backend;
 import com.google.ai.edge.litertlm.Content;
 import com.google.ai.edge.litertlm.Contents;
 import com.google.ai.edge.litertlm.Conversation;
+import com.google.ai.edge.litertlm.ConversationConfig;
 import com.google.ai.edge.litertlm.Engine;
 import com.google.ai.edge.litertlm.EngineConfig;
 import com.google.ai.edge.litertlm.Message;
+import com.google.ai.edge.litertlm.Tool;
+import com.google.ai.edge.litertlm.ToolProvider;
 import com.task82hd.LLMProvider.ILLMProvider;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.function.Function;
 
@@ -27,8 +39,35 @@ public class OnDeviceProvider extends ILLMProvider {
         AssetManager assetManager = context.getAssets();
 
         //assetManager.
+        String modelName = "gemma_4_e2b_it.litertlm";
 
-        String modelPath = "android.resource://com.task82hd/raw/gemma_4_E2B_it.litertlm";
+        String[] directoryToCheck = context.fileList();
+
+        Log.d("DeviceProvider", directoryToCheck.toString());
+
+
+
+        if(!Arrays.asList(directoryToCheck).contains("A")) {
+            try {
+                InputStream model = assetManager.open(modelName);
+
+                File out = new File(context.getFilesDir(), modelName);
+
+                OutputStream transferModel = new FileOutputStream(out);
+
+                byte[] buffer = new byte[1024];
+                int read;
+                while ((read = model.read(buffer)) != -1) {
+                    transferModel.write(buffer, 0, read);
+                }
+
+                transferModel.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        String modelPath = context.getFilesDir().getPath() + "/" + modelName;
 
 
 
@@ -46,18 +85,40 @@ public class OnDeviceProvider extends ILLMProvider {
 
         engine.initialize();
 
-        conversation = engine.createConversation(null);
+        List<ToolProvider> tools = List.of();
+        List<Message> messages = List.of();
+        Map<String, String> extraContent = Map.of();
+
+        ConversationConfig conversationConfig = new ConversationConfig(
+                Contents.Companion.of(
+
+                ),
+                messages,
+                tools,
+                null,
+                false,
+                null,
+                extraContent
+        );
+
+
+        conversation = engine.createConversation(conversationConfig);
 
 
     }
 
     @Override
     public void sendMessage(String message, Function<String, Void> callback) {
+        Map<String, String> extraContent = Map.of();
 
         Message res = conversation.sendMessage(Contents.Companion.of(
-                new Content.ImageFile(image.toString()),
+                //new Content.ImageFile(image.toString()),
                 new Content.Text(message)
-        ), null);
+        ), extraContent);
+
+        callback.apply(res.getContents().toString());
+
+        Log.d("OnDeviceLLMProvider", res.getContents().toString());
     }
 
     @Override
