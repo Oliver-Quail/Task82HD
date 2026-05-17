a76306b9dae03c13e55e802b19cda44c97338930
oliver <oliver.quail3@gmail.com>
2026-05-17 12:25:10 +1000

    Added ability to create misc config

--- app/src/main/java/com/task82hd/Database/DAO/MiscDAO.java
+++ app/src/main/java/com/task82hd/Database/DAO/MiscDAO.java
@@ -1,7 +1,17 @@
 package com.task82hd.Database.DAO;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.task82hd.Database.Entity.Misc;
 
 @Dao
 public interface MiscDAO {
+
+    @Insert
+    public abstract void createMisc(Misc misc);
+
+    @Query("SELECT * FROM misc LIMIT 1")
+    public abstract Misc getMisc();
 }
