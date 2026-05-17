ab79e5f8888e31d2111e946d314201fff6537274
oliver <oliver.quail3@gmail.com>
2026-05-17 12:19:48 +1000

    Added RAM check for running LLMs

--- app/src/main/java/com/task82hd/Fragment/SettingsFragment.java
+++ app/src/main/java/com/task82hd/Fragment/SettingsFragment.java
@@ -1,18 +1,27 @@
 package com.task82hd.Fragment;
 
+import static android.content.Context.ACTIVITY_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.task82hd.R;
 
 public class SettingsFragment extends Fragment {
 
-
+    TextView offlineModeText;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -36,5 +45,31 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        offlineModeText = view.findViewById(R.id.offline_mode_text);
+        if(canRunLLMOnDevice()) {
+            offlineModeText.setText(R.string.can_run_on_device);
+            offlineModeText.setBackgroundResource(R.drawable.background);
+        }
+    }
+
+    public boolean canRunLLMOnDevice() {
+        Context ctx = requireContext();
+
+        ActivityManager activityManager = (ActivityManager) ctx.getSystemService(ACTIVITY_SERVICE);
+
+        ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+
+        activityManager.getMemoryInfo(memoryInfo);
+
+        if(8 > memoryInfo.totalMem/(1024*1024*1024)) {
+            return false;
+        }
+
+        return true;
     }
 }
